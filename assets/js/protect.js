f53f56aaf98db067fbbfeb3c1100bfba4acd8dd4
j-street-photo <jeremy06@fastmail.fm>
2026-05-13 21:56:23 +1200

    Delete assets directory

--- assets/js/protect.js
+++
@@ -1,9 +0,0 @@
-// Disable right-click context menu on images
-document.addEventListener('DOMContentLoaded', function () {
-  document.querySelectorAll('img').forEach(function (img) {
-    img.addEventListener('contextmenu', function (e) {
-      e.preventDefault();
-      return false;
-    });
-  });
-});
